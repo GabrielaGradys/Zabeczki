7bf83efba56dafa649c62bba8d45fb4d1b8031c1
GabrielaGradys <gabriela.gradys@gmail.com>
2022-02-05 22:03:31 +0100

    Unsuccessful try to add google maps

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+  <!--  <div id="map"></div>-->
 
 </template>
 
@@ -7,6 +8,9 @@ export default {
   name: "index"
 }
 </script>
+<!--<script async-->
+<!--src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCSjxa5WEY7PGK-3g_65wMZmSy-7WkI2Kk&callback=initMap">-->
+<!--</script>-->
 
 <style scoped>
 
