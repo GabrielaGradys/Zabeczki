067922fa03c76a1e64b318ae53535aa254f4ee5a
GabrielaGradys <gabriela.gradys@gmail.com>
2022-02-06 02:44:12 +0100

    Maps Works!

---
+++ components/layout/Header.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="header" class="fixed bg-white w-full shadow px-90 flex justify-between items-center">
+<!--        <a href="/">-->
+<!--            <img :src="require('~/assets/img/1.png')" class="h-15 w-15 block" />-->
+<!--        </a>-->
+        <div id="menu" class="flex gap-16 uppercase items-center text-5 xl:text-4 tracking-wide">
+            <div v-for="(link, i) in links" :key="i" class="relative group p-4 text-center">
+                <a :href="link.link" class="hover:text-pq-orange transition-colors duration-300"> {{ link.label }} </a>
+                <div
+                    v-if="link.sublinks"
+                    class="absolute hidden gap-4 top-full bg-white w-full text-center group-hover:grid left-0"
+                >
+                    <a
+                        v-for="(sublink, j) in link.sublinks"
+                        :key="j"
+                        :href="sublink.link"
+                        class="hover:text-pq-orange transition-colors duration-300"
+                    >
+                        {{ sublink.label }}
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                {
+                    label: 'Zacznij tutaj',
+                    link: 'http://google.com',
+                    sublinks: [
+                        { label: 'O mnie', link: 'https://psychologiczneiq.pl/zacznij-tutaj/#omnie' },
+                        { label: 'Kontakt', link: 'http://google.com' },
+                    ],
+                },
+                { label: 'Psychoteka', link: 'http://google.com' },
+                { label: 'Strefa Wiedzy', link: 'http://google.com' },
+                { label: 'Z mojej perspektywy', link: 'http://google.com' },
+            ],
+        }
+    },
+}
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,84 @@
 <template>
-  <!--  <div id="map"></div>-->
-
+  <main class="grid grid-cols-1 pt-15">
+    <div id="Pin_Adder" class="flex justify-between gap-50 px-15">
+      <div
+        id="New_pin_box"
+        class="p-7 grid w-4/16 shadow bg-gradient-to-b from-pq-lightBlue to-green-500 rounded-xl tracking-wide"
+      >
+        <p
+          class="text-white text-center text-15 font-bold text-shadow --font-secondary"
+        >
+          Witaj przygodo! :) <br />
+          Chcemy Cię zamapiętać <br />
+          Dodajmy punkt na mapie
+        </p>
+        <div class="" @click="mylocation = getLocation()">TUTAJ</div>
+        <div class="" v-html="mylocation"></div>
+      </div>
+      <div
+        id="Map_Show"
+        class="p-7 flex w-full shadow bg-gradient-to-b from-pq-lightBlue to-green-400 rounded-xl tracking-wide"
+      >
+        <div id="map" class="w-full h-full"></div>
+        <!--          <iframe src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCSjxa5WEY7PGK-3g_65wMZmSy-7WkI2Kk&callback=initMap" class="w-full"></iframe>-->
+        <!--          <iframe src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed" class="w-full"></iframe>-->
+        <!--          <img src="https://ocdn.eu/pulscms-transforms/1/Y4mktkpTURBXy9iNzcwNmIwYmIzYzQyMGUxMDIzM2NmYWVlNTQ4MzQzNC5qcGeRlQMAAM0IAM0EgA" />-->
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
-  name: "index"
-}
+  name: "index",
+  data: () => ({
+    mylocation: null,
+  }),
+  mounted() {
+    const mapboxgl = require("mapbox-gl");
+    const map = new mapboxgl.Map({
+      accessToken:
+        "pk.eyJ1IjoiZ2FicmllbGEtZ3JhZHlzIiwiYSI6ImNremFqMW42eTFmcWEyb245OXAzZTl5MmsifQ.dCPrRvsSH78RNhTBPP74cQ",
+      container: "map", // <div id="map"></div>
+      style: "mapbox://styles/mapbox/streets-v9", // default style
+      center: [-21.9270884, 64.1436456], // starting position as [lng, lat]
+      zoom: 13,
+    });
+    let yourMarkersArray = [
+      { location: [-21.9270884, 64.1436456], description: "test" },
+    ];
+    yourMarkersArray.map((marker) => {
+      const LngLat = marker.location;
+      const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+        marker.description
+      );
+      new mapboxgl.Marker().setLngLat(LngLat).setPopup(popup).addTo(map); // Initialized above
+    });
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true,
+      })
+    );
+  },
+  methods: {
+    getLocation() {
+      let position = "no gps";
+      function save(pos) {
+        position = pos;
+      }
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(save);
+      }
+      return position;
+    },
+  },
+};
 </script>
-<!--<script async-->
-<!--src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCSjxa5WEY7PGK-3g_65wMZmSy-7WkI2Kk&callback=initMap">-->
-<!--</script>-->
 
-<style scoped>
-
-</style>
+<style scoped></style>
